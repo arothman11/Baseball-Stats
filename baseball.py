4b2c88f528b168d6226d3f1084f80967f4f5bb23
ameerothman <arothman@wustl.edu>
2021-02-25 20:09:32 -0600

    done!

--- baseball.py
+++ baseball.py
@@ -1,6 +1,18 @@
 import re
 import sys, os
+import operator
 
+players = dict()
+
+class player:
+    def __init__(self, name):
+        self.name = name
+        self.bats = 0
+        self.hits = 0
+        self.avg = 0
+   
+    def output(self):
+        return f"{self.name}: {self.avg}"
 
 baseball_regex = re.compile(r"(\w* \w*) batted (\d+) times with (\d+) hits and \d+ runs")
 
@@ -15,3 +27,24 @@ if not os.path.exists(filename):
 with open(filename) as f:
     for line in f:
         match = re.match(baseball_regex, line)
+        if match is not None:
+            name = match.group(1)
+            bat = match.group(2)
+            hit = match.group(3)
+            
+            if name not in players.keys():
+                players[name] = player(name)
+            
+            players[name].bats += int(bat)
+            players[name].hits += int(hit)
+    
+for name in players:
+    players[name].avg = players[name].hits/players[name].bats
+    #https://docs.python.org/3/tutorial/floatingpoint.html
+    players[name].avg = format(players[name].avg, '.3f')
+
+#https://stackoverflow.com/questions/4010322/sort-a-list-of-class-instances-python
+sorted_players = sorted(players.values(), key=operator.attrgetter('avg'), reverse=True)
+
+for player in sorted_players:
+    print (player.output())
